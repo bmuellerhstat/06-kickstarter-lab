a450a29f3b39327290b291c4ba406538e68f1b3f
Kelly Lai <zhiyinl5633@hstat.org>
2018-01-12 03:40:06 +0000

    done kickstarter

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,6 @@ class Backer
     
     def back_project(project)
         @backed_projects << project
-        project.backers << self #applies to backer
+        project.backers << self #bker
     end
 end
