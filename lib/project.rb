57111ad98a2645237a674877f2f9bb6cc0b1389a
stephenw6888 <stephenw6888@hstat.org>
2018-01-12 01:52:52 +0000

    finished

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+    
+    attr_accessor :title, :backers
+    
+    def initialize(title=[],backers=[])
+        @title = title 
+        @backers = backers
+    end    
+    
+    def add_backer(name)
+        @backers << name
+        name.backed_projects << self
+    end
+    
+end
