cb6bde08fff5cc31a3995a5714d2073b6e696847
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-12-21 23:11:14 +0000

    done

---
+++ lib/backer.rb
@@ -0,0 +1,30 @@
+class Backer
+   attr_accessor :name, :backed_projects
+   def initialize(name)
+      @name = name 
+      @backed_projects = []
+      
+   end
+   def back_project(project)
+       @backed_projects << project
+       project.backers << self
+   end
+end
+class Project
+    attr_accessor :title, :backers
+    def initialize(title)
+       @title = title 
+       @backers = []
+    end
+    def add_backer(backer)
+        @backers << backer
+        backer.backed_projects << self
+    end
+end
+
+
+# bob.backed_projects
+# # => #<Project:0x000001018683d0 @title="This is an Awesome Project"...>
+
+# awesome_project.backers
+# => #<Backer:0x000001018b9370 @name="Bob"...>
